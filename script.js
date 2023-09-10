94fd1cf1c94923141737fd53fa67f7ad29c531a1
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-09-10 14:08:32 -0700

    zuri commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/** @format */
+function currentDayAndTime() {
+  const date = new Date();
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = weekdays[date.getUTCDay()];
+  
+  const currentUTCTime = Date.now();
+  const formattedTime = new Date(currentUTCTime).toISOString().substr(11, 12);
+  
+  // Get currentDayOfTheWeek and currentUTTime
+  const today = document.getElementById("currentDayOfTheWeek");
+  today.innerHTML = "Today is: " + dayOfWeek;
+  const Time = document.getElementById("currentUTCTime");
+  Time.innerHTML = currentUTCTime;
+  }
+  
+  // Update every 100 milliseconds (0.1 seconds)
+  setInterval(currentDayAndTime, 100); 
+  
+  // Call the function initially
+  currentDayAndTime();
